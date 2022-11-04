c870b132b3bca3a8cc58c9cec73ea17fa4f4fd16
Daniel <danielmontrest@gmail.com>
2022-11-04 16:10:25 -0500

    Se envia clase 20 sin errores, aun falta agregar las posibles respuestas de las preguntas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path 
 from . import views
 
+'''los corchetes angulares <> para pasar parametros variables mediante la url,
+o sea, entre los corchetes se pone el parametro question_id por lo tanto como esto es
+una anidacion de polls/ entonces al ingresar al polls/4 o cualuqiern umero de id, se
+estaria entrando a la pregunta deseada '''
 urlpatterns = [
-    path("", views.index, name="index")
+    #ex polls/
+    path("", views.index, name="index"),
+    #ex polls/5
+    path("<int:question_id>/", views.detail, name="index"),
+    #ex polls/5/results
+    path("<int:question_id>/results/", views.results, name="index"),
+    #ex polls/5/votes
+    path("<int:question_id>/vote/", views.vote, name="index"),
+
 ]
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
-
-# Create your views here.
+'''A continuacion se crean 4 viastas basadas en funciones 
+(function base views
+clase 21: ademas se incluye en el rtrun, la template correspondiente creada en el archivo de templates
+'''
 def index(request):
-    return HttpResponse("Hello Word")
+    lates_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": lates_question_list
+    })
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando la pregunta número {question_id}")
